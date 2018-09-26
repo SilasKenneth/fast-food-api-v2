070965894ed25c8817811030d0a81a5a063efeeb
SilasKenneth <silaskenneth1@gmail.com>
2018-09-27 01:25:12 +0300

    [CHORE] Create the user resource file

---
+++ app/resources/users.py
@@ -0,0 +1,22 @@
+from flask_restful import Resource
+
+
+class SignUpResource(Resource):
+    def post(self):
+        pass
+
+
+class LoginResource(Resource):
+    def post(self):
+        pass
+
+
+class ProfileResource(Resource):
+    def put(self):
+        pass
+
+    def get(self):
+        pass
+
+    def delete(self):
+        pass
