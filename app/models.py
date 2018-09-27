e432c5b30a26c782a5a0363fd645ffe329910e48
SilasKenneth <silaskenneth1@gmail.com>
2018-09-27 17:13:46 +0300

    [CHORE] Add a method to get a user by email and remove password from json

--- app/models.py
+++ app/models.py
@@ -24,28 +24,73 @@ class Base(object):
 
 class User(Base):
 
-    def __init__(self, username, password, email):
+    def __init__(self, fullnames, username, password, email):
         super(User, self).__init__()
         self.id = None
+        self.fullnames = fullnames
         self.username = username
         self.email = email
         self.password = generate_password_hash(password)
 
     def save(self):
         try:
+            fullnames = self.fullnames
             username = self.username
             password = self.password
             email = self.email
-            sql = "INSERT INTO users(username, email, password)values('%s', '%s', '%s')" % (username, email, password)
+            sql = "INSERT INTO users(fullnames, username, email, password)values('%s', '%s', '%s', '%s')" % \
+                  (
+                      fullnames,
+                      username,
+                      email,
+                      password
+                  )
             if self.conn is None:
                 return False
             self.cursor.execute(sql)
             self.conn.commit()
             return True
         except Exception as ex:
+            print(ex)
             self.conn.rollback()
             return False
-
+    @property
+    def json(self):
+        return {
+            "fullnames": self.fullnames,
+            "username": self.username,
+            "email": self.email
+        }
+    @classmethod
+    def get_by_username_or_email(cls, username):
+        try:
+            sql = "SELECT * FROM users WHERE username = '%s' or email = '%s'"%(
+                username,
+                username
+            )
+            if cls.conn is None:
+                return None
+            cls.cursor.execute(sql)
+            record = cls.cursor.fetchone()
+            if not record:
+                return None
+            found = User(
+                username=record[1],
+                password=record[4],
+                email=record[3],
+                fullnames=record[2]
+            )
+            found.id = record[0]
+            found.password = record[4]
+            return found
+        except Exception as ex:
+            print(ex)
+            return None
+    def get_orders(self):
+        try:
+            pass
+        except Exception as ex:
+            return {}
 
 class Menu(Base):
     def __init__(self, name, description, price):
