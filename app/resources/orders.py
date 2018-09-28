810c9e006dcaec3979fa992790967be64b7d39d2
SilasKenneth <silaskenneth1@gmail.com>
2018-09-28 13:49:06 +0300

    [CHORE] Test the access token required decorator

--- app/resources/orders.py
+++ app/resources/orders.py
@@ -1,5 +1,8 @@
 from flask_restful import Resource
 
+from app.models import Order
+from app.utils import admin_token_required
+
 
 class OrderResource(Resource):
     def post(self):
@@ -11,10 +14,12 @@ class OrderResource(Resource):
     def put(self):
         pass
 class AdminOrderResource(Resource):
+    @admin_token_required
     def get(self, order_id=None):
         if order_id is None:
             pass
-        order = ""
+        order = Order.find_by_id(ids=order_id)
+        return order
     def post(self):
         pass
     def put(self, order_id):
