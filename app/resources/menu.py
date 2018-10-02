bef5d4efb34a51436092735e3b5be61016a85b97
SilasKenneth <silaskenneth1@gmail.com>
2018-10-02 16:35:29 +0300

    [CHORE] Add more files to the base

--- app/resources/menu.py
+++ app/resources/menu.py
@@ -1,6 +1,11 @@
 from flask_restful import Resource, reqparse
 from app.models import Menu
-from app.utils import empty, valid_price, valid_name, valid_description
+from app.utils import (empty,
+                       valid_price,
+                       valid_name,
+                       valid_description,
+                       decode_token as claims,
+                       admin_token_required)
 
 
 class MenuResource(Resource):
@@ -9,6 +14,7 @@ class MenuResource(Resource):
     parser.add_argument("description", required=True, help="Please provide a description")
     parser.add_argument("price", required=True, help="Please provide a price")
 
+    @admin_token_required
     def post(self):
         args = self.parser.parse_args()
         name = args.get("name", "")
@@ -33,19 +39,52 @@ class MenuResource(Resource):
                 menu_item.json}, 201
         return {"message": "There was problem saving the item. Try again"}, 403
 
+    @admin_token_required
     def put(self, menu_id):
-        pass
 
-    def get(self, meal_id=None):
-        if meal_id is None:
+        menu = Menu.find_by_id(meal_id=menu_id)
+        if menu is None:
+            return {"message": "The menu item with an id number %s doesn't"
+                               " exist"%menu_id}, 404
+        args = self.parser.parse_args()
+        name = args.get("name", "")
+        description = args.get("description", "")
+        price = args.get("price", "")
+        if empty(name) or empty(description) or empty(price):
+            return {"message": "You have to specify all"
+                               " details of an item"
+                               " such as name, price, description"}, 403
+        if not valid_name(name):
+            return {"message": "Please specify a valid name for item"}, 403
+        if not valid_price(price):
+            return {"message": "Please specify a valid price"
+                               " for the item"}, 403
+        if not valid_description(description):
+            return {"message": "Please specify a valid description "
+                               "for the item"}
+        menu_item = Menu(name, description, 200)
+        menu_item.id = menu.id
+        updated = menu_item.update()
+        if updated:
+            return {"message": "The menu item was successfully updated",
+                    "menu_item": menu_item.json}, 200
+        else:
+            return {"message": "There was a problem updating the menu item"
+                               " maybe you provided a name already taken by"
+                               " another menu item"}, 400
+
+    @admin_token_required
+    def get(self, menu_id=None):
+        if menu_id is None:
             meals = Menu.all()
             return {"menu": meals}, 200
-        if not isinstance(meal_id, str) and not isinstance(meal_id, int):
-            return {"message": "The menu item with id %s does not exist" % meal_id}, 404
-        meal = Menu.find_by_id(meal_id)
+        if not isinstance(menu_id, str) and not isinstance(menu_id, int):
+            return {"message": "The menu item with id %s does not exist" % menu_id}, 404
+        meal = Menu.find_by_id(meal_id=menu_id)
         if not meal:
-            return {"message": "The menu item with id %s does not exist" % meal_id}, 404
+            return {"message": "The menu item with id %s does not exist" % menu_id}, 404
         return {"item": meal.json}
 
+    @admin_token_required
     def delete(self, meal_id):
         pass
--- app/resources/orders.py
+++ app/resources/orders.py
@@ -1,21 +1,72 @@
-from flask_restful import Resource
+from flask import request
+from flask_restful import Resource, reqparse
+
+<<<<<<< Updated upstream
+from app.models import Order, Address, Menu
+from app.utils import (admin_token_required,
+                       normal_token_required,
+                       decode_token as claims,
+                       empty)
 
 
 class OrderResource(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument("address", required=True, help="Please provide "
+                                                       "an address for delivery")
+    parser.add_argument("items", required=True, help="Please specify a comma separated list of"
+                                                     " items")
+
+    @normal_token_required
     def post(self):
-        pass
+        print(request.authorization)
+        args = self.parser.parse_args()
+        address_id = args.get("address", "")
+        items = args.get("items", "")
+        if empty(address_id) or empty(items):
+            return {"message": "Please specify an address and a list of"
+                               " items"}, 400
+        address = Address.find_by_id(address_id=address_id)
+        if not address:
+            return {"message": "The address you provided is not valid"
+                               " please check your addresses and give a"
+                               " valid id"}, 400
+        items = items.split(",")
+        ordered_items = []
+        for item in items:
+            item_object = Menu.find_by_id(meal_id=item)
+            if item_object is None:
+                return {"message": "The item with id %s does not exist."
+                                   ". Try another" %
+                                   item}, 404
+            ordered_items.append(item_object)
+        return {"message": "The order was successfully placed", "order": order.json1}, 200
 
-    def get(self):
-        pass
-
-    def put(self):
-        pass
-class AdminOrderResource(Resource):
+    @normal_token_required
     def get(self, order_id=None):
+        if order_id is None:
+            return Order.all()
+        return Order.find_by_id(ids=order_id)
+
+    @normal_token_required
+    def put(self, order_id=None):
+        pass
+
+
+class AdminOrderResource(Resource):
+    """The resource where admin manages orders"""
+
+    @admin_token_required
+    def get(self, order_id=None):
+        """Get all orders if order_id is None otherwise get a specific order"""
         if order_id is None:
             pass
         order = ""
-    def post(self):
-        pass
     def put(self, order_id):
         pass
+            orders = Order.all()
+            result = [order.json for order in orders]
+            return {"message": "success", "orders": result}, 200
+        order = Order.find_by_id(ids=order_id)
+        if not order:
+            return {"message": "The order with id %s is not available" % order_id}, 404
+        return order.json1
