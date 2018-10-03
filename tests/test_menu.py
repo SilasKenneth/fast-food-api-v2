9755d7e0fc1621d43dd0fc01fe4f872d8f7cd9db
SilasKenneth <silaskenneth1@gmail.com>
2018-10-03 13:49:13 +0300

    [FIX] Add the Authorization header for the requests

--- tests/test_menu.py
+++ tests/test_menu.py
@@ -5,7 +5,8 @@ from tests.base import BaseTest
 
 class TestMenu(BaseTest):
     def test_can_add_menu_item(self):
-        response = self.client.post(self.MENU_URL, data=json.dumps(self.sample_meal), content_type="application/json")
+        response = self.client.post(self.MENU_URL, data=json.dumps(self.sample_meal), content_type="application/json",
+                                    headers=self.get_admin_headers())
         response_obj = toobj(response.data)
         self.assertNotEqual(response_obj, None)
         self.assertEqual(response_obj.get("message", None), "The menu item was successfully saved")
@@ -13,6 +14,7 @@ class TestMenu(BaseTest):
 
     def test_cannot_add_empty_name(self):
         response = self.client.post(self.MENU_URL, data=json.dumps(self.meal_without_name),
+                                    headers=self.get_admin_headers(),
                                     content_type="application/json")
         response_obj = toobj(response.data)
         self.assertNotEqual(response_obj, None)
@@ -22,7 +24,7 @@ class TestMenu(BaseTest):
 
     def test_cannot_add_empty_description(self):
         response = self.client.post(self.MENU_URL, data=json.dumps(self.meal_with_no_description),
-                                    content_type="application/json")
+                                    content_type="application/json", headers=self.get_admin_headers())
         response_obj = toobj(response.data)
         self.assertNotEqual(response_obj, None)
         self.assertEqual(response_obj.get("message", None),
@@ -31,21 +33,43 @@ class TestMenu(BaseTest):
 
     def test_cannot_add_invalid_price(self):
         response = self.client.post(self.MENU_URL, data=json.dumps(self.meal_with_invalid_price),
-                                    content_type="application/json")
+                                    content_type="application/json", headers=self.get_admin_headers())
         response_obj = toobj(response.data)
         self.assertNotEqual(response_obj, None)
         self.assertEqual(response_obj.get("message", None), "Please specify a valid price for the item")
         self.assertEqual(response.status_code, 403)
 
     def test_cannot_add_existing(self):
-        response = self.client.post(self.MENU_URL, data=json.dumps(self.sample_meal), content_type="application/json")
+        response1 = self.client.post(self.MENU_URL, data=json.dumps(self.sample_meal),
+                                     content_type="application/json",
+                                     headers=self.get_admin_headers())
+        response = self.client.post(self.MENU_URL, data=json.dumps(self.sample_meal),
+                                    content_type="application/json",
+                                    headers=self.get_admin_headers())
         response_obj = toobj(response.data)
         self.assertNotEqual(response_obj, None)
         self.assertEqual(response_obj.get("message", None), "There was problem saving the item. Try again")
         self.assertEqual(response.status_code, 403)
+
     def test_cannot_add_invalid_name(self):
-        response = self.client.post(self.MENU_URL, data=json.dumps(self.sample_meal), content_type="application/json")
+        response = self.client.post(self.MENU_URL, data=json.dumps(self.sample_meal), content_type="application/json",
+                                    headers=self.get_admin_headers())
         response_obj = toobj(response.data)
         self.assertNotEqual(response_obj, None)
         self.assertEqual(response_obj.get("message", None), "There was problem saving the item. Try again")
         self.assertEqual(response.status_code, 403)
+
+    def test_cannot_add_without_token(self):
+        response = self.client.post(self.MENU_URL, data=json.dumps(self.sample_meal), content_type="application/json")
+        response_obj = toobj(response.data)
+        self.assertNotEqual(response_obj, None)
+        self.assertEqual(response_obj.get("message", None), "An error occurred while decoding token.")
+        self.assertEqual(response.status_code, 400)
+
+    def test_cannot_add_without_admin_token(self):
+        response = self.client.post(self.MENU_URL, data=json.dumps(self.sample_meal), content_type="application/json",
+                                    headers=self.logged_in_user())
+        response_obj = toobj(response.data)
+        self.assertNotEqual(response_obj, None)
+        self.assertEqual(response_obj.get("message", None), "An error occurred while decoding token.")
+        self.assertEqual(response.status_code, 400)
