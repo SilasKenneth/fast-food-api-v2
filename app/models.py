22eaae75ff70e35c0847935983e21af20d3bc154
SilasKenneth <silaskenneth1@gmail.com>
2018-10-04 12:10:21 +0300

    [FIX #160982498] Fix issues with user identity and remove unsused exception variables

--- app/models.py
+++ app/models.py
@@ -12,11 +12,10 @@ class Base(object):
 
     def __init__(self):
         self.conn = database.connection
-        print(self.conn)
         self.cursor = database.cursor
 
     @classmethod
-    def all(cls, table, token=None):
+    def all(cls, table, user_id=None, user_type="0"):
         try:
             sql = "SELECT * FROM %s order by id" % (table)
             if cls.conn is None:
@@ -24,13 +23,13 @@ class Base(object):
             cls.cursor.execute(sql)
             records = cls.cursor.fetchall()
             return records
-        except psycopg2.Error as ex:
-            print(ex)
+        except psycopg2.Error:
+            # print(ex)
             cls.conn.rollback()
             return []
 
     @classmethod
-    def find_by_id(cls, table_name, ids, token=None):
+    def find_by_id(cls, table_name, ids, user_id=""):
         try:
             if not str(ids).isnumeric():
                 return []
@@ -41,8 +40,8 @@ class Base(object):
             cls.cursor.execute(sql)
             record = cls.cursor.fetchone()
             return record
-        except Exception as ex:
-            print(ex)
+        except Exception:
+            # print(ex)
             return []
 
 
@@ -79,8 +78,8 @@ class User(Base):
             self.cursor.execute(sql)
             self.conn.commit()
             return True
-        except Exception as ex:
-            print(ex)
+        except Exception:
+            # print(ex)
             self.conn.rollback()
             return False
 
@@ -94,6 +93,7 @@ class User(Base):
             "email": self.email,
             "user_type": "admin" if self.user_type == '0' else "normal"
         }
+
     @property
     def json1(self):
         """Remove the id field from the serializable version"""
@@ -124,14 +124,13 @@ class User(Base):
                 email=str(record[3]),
                 fullnames=str(record[2])
             )
-            # print(found)
             found.id = str(record[0])
             found.password = str(record[5])
             found.user_type = str(record[4])
             return found
-        except Exception as ex:
+        except Exception:
             # print("Some stupid error occurs here")
-            print(ex)
+            # print(ex)
             return None
 
     def get_orders(self):
@@ -139,7 +138,7 @@ class User(Base):
             sql = "SELECT * FROM orders WHERE user_id = '%s' order by id" % (
                 self.id
             )
-        except Exception as ex:
+        except Exception:
             return {}
 
 
@@ -157,6 +156,7 @@ class Menu(Base):
         self.description = description
         self.price = price
         self.date_added = datetime.datetime.utcnow()
+        self.quantity = 1
 
     def save(self):
         """Save a menu item to the database
@@ -172,7 +172,7 @@ class Menu(Base):
             self.cursor.execute(sql)
             self.conn.commit()
             return True
-        except Exception as ex:
+        except Exception:
             # print(ex)
             self.conn.rollback()
             return False
@@ -184,7 +184,7 @@ class Menu(Base):
         return menus.json, menus
 
     @classmethod
-    def all(cls, token=None, **kwargs):
+    def all(cls, user_id=None, user_type='0', **kwargs):
         items = super(Menu, cls).all("meals")
         res = []
         for item in items:
@@ -209,9 +209,9 @@ class Menu(Base):
         }
 
     @classmethod
-    def find_by_id(cls, table_name="", meal_id="", token=None):
+    def find_by_id(cls, table_name="", meal_id="", user_id=None):
         """Find a meal by a meal_id"""
-        menu_item = super(Menu, cls).find_by_id("meals", meal_id, token)
+        menu_item = super(Menu, cls).find_by_id("meals", meal_id, user_id)
         if not menu_item:
             return None
         return cls.format(menu_item)[-1]
@@ -227,15 +227,17 @@ class Menu(Base):
             if self.conn is None:
                 return False
             self.cursor.execute(sql)
+            self.conn.commit()
             return True
-        except psycopg2.Error as ex:
-            print(ex)
+        except psycopg2.Error:
+            # print(ex)
             self.conn.rollback()
             return False
 
 
 class Order(Base):
     """The order class which represents an order"""
+
     def __init__(self, customer_id, address_id, items):
         """Constructor for the Order class"""
         super(Order, self).__init__()
@@ -246,72 +248,171 @@ class Order(Base):
         self.address = address_id
         self.date_ordered = datetime.datetime.utcnow()
         self.status = "pending"
+        self.total = 0.00
 
     def save(self):
         try:
-            sql = "INSERT INTO orders(reference, customer_id, date_ordered, status)VALUES" \
-                  " ('%s', '%s', '%s', '%s')" % (
+            sql1 = "SELECT MAX(id) FROM orders"
+            sql = "INSERT INTO orders(reference, user_id, date_ordered, address_id, status)VALUES" \
+                  " ('%s', '%s', now(), '%s' ,'%s')" % (
                       self.reference,
                       self.customer_id,
-                      self.date_ordered,
+                      self.address,
                       self.status
                   )
             if self.conn is None:
                 return False
+            self.cursor.execute(sql1)
+            nexter = self.cursor.fetchone()
+            num = 1
+            if not nexter:
+                num = 1
+            else:
+                num = nexter[0]
             self.cursor.execute(sql)
-            nexter = self.next_id()
+            if num is None:
+                self.conn.commit()
+                self.cursor.execute(sql1)
+                nexter = self.cursor.fetchone()
+                num = nexter[0]
+            print(num, "Something here")
+            num = (int(num) + 1) if (num != 1 or num is None) else num
             for item in self.items:
-                sql_indivi = "INSERT INTO order_meals(meal_id, order_id) VALUES ('%s', '%s')" % (
+                sql_individual = "INSERT INTO order_meals(meal_id, order_id, quantity, cost) VALUES ('%s', '%s', '%s', '%s')" % (
                     item.id,
-                    nexter
+                    num,
+                    item.quantity,
+                    item.price
                 )
-                self.cursor.execute(sql_indivi)
+                self.cursor.execute(sql_individual)
             self.conn.commit()
-        except Exception as ex:
+            return True
+        except psycopg2.Error as ex:
+            print(ex)
             self.conn.rollback()
             return False
 
     @classmethod
-    def all(cls, table="orders", token=""):
-        records = super(Order, cls).all(table)
-        who = claims(token)
-        if not who:
-            return []
+    def format(cls, order):
+        """Format an order"""
+        try:
+            order_gotten = Order(order[2], order[3], order[4])
+            order_gotten.date_ordered = order[3]
+            order_gotten.id = order[0]
+            order_gotten.total = cls.get_total(order_gotten.id)
+            print(order_gotten.id)
+            return order_gotten
+        except IndexError:
+            return None
+
+    @classmethod
+    def all(cls, table="orders", user_id="", user_type='0'):
+        """Get all orders"""
+        records = super(Order, cls).all(table, user_id=user_id, user_type=user_type)
         res = []
-        if who.get("user_type", "") == "admin":
+        if int(user_type) == 0:
+            records = [cls.format(record) for record in records]
             return records
         for record in records:
-            if record[1] == who.get("id", 0):
-                res.append(record)
+            if int(record[2]) == int(user_id):
+                res.append(cls.format(record))
         if not res:
             return []
-        print(res)
         return res
 
     @classmethod
-    def find_by_id(cls, table_name="orders", ids="", token=""):
-        order = super(Order, cls).find_by_id(table_name, ids, token)
-        if not order:
-            return {}
-        return {}
+    def get_total(cls, order_id):
+        """Get the total value of an order given the id"""
+        try:
+            sql = "SELECT sum(cost * quantity) FROM order_meals WHERE order_id='%s'" % (
+                order_id
+            )
+            if cls.conn is None:
+                return 0.00
+            cls.cursor.execute(sql)
+            record = cls.cursor.fetchone()
+            if not record:
+                return 0.00
+            return record[0]
+        except Exception:
+            return 0.00
 
     @classmethod
-    def next_id(cls):
+    def find_by_id(cls, table_name="orders", ids="", user_id=""):
+        """Get an order given the id"""
+        order = super(Order, cls).find_by_id(table_name, ids, user_id)
+        if not order:
+            return None
+        order = cls.format(order)
+        # print(order.customer_id)
+        if not str(user_id).isnumeric():
+            return []
+        if user_id == '1' or user_id == 1:
+            user_id = 1
+        user = User.find_by_id("users", ids=1)
+        if not user:
+            return []
+        if user[4] == '0' or user[4] == 0:
+            return order
+        if int(user_id) != int(order.customer_id):
+            return []
+        return order
+    @property
+    def json(self):
+        """Return a json serializable version of the order"""
+        return {
+            "id": str(self.id),
+            "reference": str(self.reference),
+            "date_ordered": str(self.date_ordered),
+            "value": str(self.total),
+            "status": str(self.status)
+        }
+
+    @property
+    def json1(self):
+        """Return a json serializable version of the order"""
+        return {
+            "reference": str(self.reference),
+            "status": str(self.status),
+            "date_ordered": str(self.date_ordered),
+            "items": Order.get_items(order_id=self.id),
+            "total": self.get_total(self.id)
+        }
+
+    @classmethod
+    def get_items(cls, order_id):
+        """Get the total value of an order given the id"""
         try:
-            sql = "SELECT nextval(id) FROM users"
+            sql = "SELECT * FROM order_meals WHERE order_id='%s'" % (
+                order_id
+            )
             if cls.conn is None:
-                return 1
+                return 0.00
             cls.cursor.execute(sql)
-            res = cls.cursor.fetchone()
-            if not res:
-                return 1
-            return res[0]
+            record = cls.cursor.fetchall()
+            if not record:
+                return 0.00
+            record = list(record)
+            res = []
+            for item in record:
+                res.append(cls.format_menu_order(item))
+            return res
         except Exception as ex:
-            return 1
+            print(ex)
+            return []
+
+    @classmethod
+    def format_menu_order(cls, menu_order):
+        return {
+            "menu_item": Menu.find_by_id(meal_id=menu_order[1]).name,
+            "quantity": menu_order[3],
+            "unit_price": menu_order[4]
+        }
 
 
 class Address(Base):
     def __init__(self, town, street, phone):
+        """The constructor for the addresses class"""
         super(Address, self).__init__()
         self.id = None
         self.town = town
@@ -320,16 +421,25 @@ class Address(Base):
         self.user_id = None
 
     @classmethod
-    def find_by_id(cls, table_name="addresses", address_id="", token=None):
+    def find_by_id(cls, table_name="addresses", address_id="", user_id=""):
+        """Find an address by a given if"""
         address = super(Address, cls).find_by_id(
-            table_name="addresses", ids=address_id, token=token)
-        who_this_is = claims(token)
+            table_name="addresses", ids=address_id)
+        if not address:
+            return []
+        new_add = cls.format(address)
+        if new_add is None:
+            return []
+        print(new_add)
+        if int(new_add.user_id) != int(user_id):
+            return []
         return address
-    def save(self, token=""):
+
+    def save(self, user_id=""):
         """Save the address to the database"""
         try:
-            sql = "INSERT INTO addresses(user_id, town, phone, street)values('%s', '%s', '%s')"%(
-                self.user_id,
+            sql = "INSERT INTO addresses(user_id, town, phone, street)values('%s', '%s', '%s', '%s')" % (
+                user_id,
                 self.town,
                 self.phone,
                 self.street
@@ -337,6 +447,38 @@ class Address(Base):
             if self.conn is None:
                 return False
             self.cursor.execute(sql)
-
-        except Exception as ex:
+            self.conn.commit()
+            return True
+        except Exception:
+            # print(ex)
             return False
+
+    @classmethod
+    def format(cls, fields):
+        """Format the address to an object"""
+        address = Address(fields[2], fields[3], fields[4])
+        address.id = fields[0]
+        address.user_id = fields[1]
+        return address
+
+    @property
+    def json(self):
+        """Return json serializable properties without id"""
+        return {"town": self.town, "street": self.street, "phone": self.phone}
+
+    @property
+    def json1(self):
+        """Return JSON serializable properties without id"""
+        return {"id": self.id, "town": self.town, "street": self.street, "phone": self.phone}
+
+    @classmethod
+    def all(cls, table_name="addresses", user_id="0", user_type='0'):
+        """Return all the addresses for a specific user"""
+        records = super(Address, cls).all("addresses", user_id=user_id)
+        if not records:
+            return []
+        res = []
+        for record in records:
+            if int(record[1]) == int(user_id):
+                res.append(cls.format(fields=record))
+        return res
