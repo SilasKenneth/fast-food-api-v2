090bd48d8485859b2aa938bcae494f1f20f0eb23
SilasKenneth <silaskenneth1@gmail.com>
2018-10-04 10:40:31 +0300

    [CHORE] Create the addresses resource

--- app/resources/addresses.py
+++ app/resources/addresses.py
@@ -1,9 +1,17 @@
-from flask_restful import Resource
-
+from flask_restful import Resource, reqparse
+from app.utils import normal_token_required
 class AddressResource(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument("town", required=True, help="Please provide a town")
+    parser.add_argument("street", required=True, help="Please provide a street")
+    parser.add_argument("phone", required=True, help="Please provide a phone")
+
+    @normal_token_required
     def get(self, address_id = None):
         pass
+    @normal_token_required
     def post(self):
         pass
+    @normal_token_required
     def put(self, address_id = None):
         pass
