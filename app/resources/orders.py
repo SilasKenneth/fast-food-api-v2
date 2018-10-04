1ca4e4a4f4bcc77a61fe679e9207e12fd37b3256
SilasKenneth <silaskenneth1@gmail.com>
2018-10-04 12:39:52 +0300

    [FIX] Fix issues with the user_type verification from token

--- app/resources/orders.py
+++ app/resources/orders.py
@@ -19,41 +19,65 @@ class OrderResource(Resource):
     def post(self):
         """Create a new order"""
         args = self.parser.parse_args()
-        # import pdb;
-        # pdb.set_trace()
         address_id = args.get("address", "")
         items = args.get("items", "")
-
+        token = request.headers.get("Authorization", "")
+        token = token.split(" ")
+        if len(token) < 2:
+            return {"message": "You could not be verified please login and try again"}, 403
+        token = token[1]
+        customer_id = claims(token).get("id", "0")
+        print(claims(token))
         if empty(address_id) or empty(items):
             return {"message": "Please specify an address and a list of"
                                " items"}, 400
-        address = Address.find_by_id(address_id=address_id)
+        address = Address.find_by_id(address_id=address_id, user_id=customer_id)
         if not address or len(address) == 0:
             return {"message": "The address you provided is not valid"
                                " please check your addresses and give a"
                                " valid id"}, 400
         items = items.split(",")
-
+        new_items = [int(item) for item in items if item.strip() != '']
+        copy_items = set(new_items)
+        new_items = list(new_items)
         ordered_items = []
-        for item in items:
+        total = 0.00
+        for item in copy_items:
             item_object = Menu.find_by_id(meal_id=item)
             if item_object is None:
                 return {"message": "The item with id %s does not exist."
                                    ". Try another" %
                                    item}, 404
+            item_object.quantity = new_items.count(item)
+            total += float(item_object.price * item_object.quantity)
             ordered_items.append(item_object)
-        order = Order()
+        order = Order(customer_id, address_id, ordered_items)
+        order.total = total
+        saved = order.save()
+        if not saved:
+            return {"message": "There was a problem placing the order please try again"}, 400
         return {"message": "The order was successfully placed", "order": order.json1}, 200
 
     @normal_token_required
     def get(self, order_id=None):
         """Get orders if not order_id is provided otherwise get a given order if it belongs to the user"""
+        token = request.headers.get("Authorization", "")
+        token = token.split(" ")
+        if len(token) < 2:
+            return {"message": "You could not be verified please login and try again"}, 403
+        token = token[1]
+        customer_id = claims(token).get("id", "0")
         if order_id is None:
-            orders = Order.all()
+            orders = Order.all(user_id=customer_id, user_type=1)
             if not orders:
                 return {"message": "You don't currently have any orders"}, 404
-        order = Order.find_by_id(ids=order_id)
-        return order.json
+            orders = [order.json for order in orders]
+            return {"message":"success", "orders":orders}
+        order = Order.find_by_id(ids=order_id, user_id=customer_id)
+        if not order:
+            return {"message": "The order was not found in the database "
+                               ".Try again"}, 404
+        return {"message": "success", "order": order.json1}, 200
 
     @normal_token_required
     def put(self, order_id=None):
@@ -74,6 +98,8 @@ class OrderResource(Resource):
 
 class AdminOrderResource(Resource):
     """The resource where admin manages orders"""
+    parser = reqparse.RequestParser()
+    parser.add_argument("status", required=True, help="Please provide a status")
 
     @admin_token_required
     def get(self, order_id=None):
@@ -81,12 +107,12 @@ class AdminOrderResource(Resource):
         token = request.headers.get("Authorization", "")
         token = token.split(" ")
         token = token[-1]
-        print(claims(token))
+        details = claims(token)
         if order_id is None:
             orders = Order.all()
             result = [order.json for order in orders]
             return {"message": "success", "orders": result}, 200
-        order_current = Order.find_by_id(ids=order_id)
+        order_current = Order.find_by_id(ids=order_id, user_id=details.get("id", "1"))
         if not order_current:
             return {"message": "The order with id %s is not available" % order_id}, 404
         return order_current.json1
@@ -94,10 +120,15 @@ class AdminOrderResource(Resource):
     @admin_token_required
     def put(self, order_id=None):
         """Update the status of an order by cancelling it"""
+        args = self.parser.parse_args()
+        status = args.get("status", "")
         if order_id is None:
             return {"message": "Missing order_id in your url"}, 400
+        if empty(status):
+            return {"message": "Please provide a status to change the order %s's status to" % order_id}, 403
         order = Order.find_by_id(ids=order_id)
         if order is None:
             return {"message": "The order with order_id %s was not "
                                "found" % order_id}, 404
+        order.status = status
         return order
